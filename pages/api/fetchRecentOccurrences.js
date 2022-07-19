4c3d518c98b8700e05823c3c88e5550a2ef5b27e
Arthur <arthur.mello.marques@gmail.com>
2022-07-19 14:20:43 -0300

    feat: add api route to fetch recent occurrences

---
+++ pages/api/fetchRecentOccurrences.js
@@ -0,0 +1,24 @@
+export default function handler(req, res) {
+  res.status(200).json({
+    occurrences: [ 
+      {
+        id: 0,
+        petName: 'Saul Goodboy',
+        author: 'Saul Goodman',
+        lat: 0,
+        lon: 0,
+        description: 'good boy',
+        lost: 0,
+      },
+      {
+        id: 1,
+        petName: 'Sabrina Goodgirl',
+        author: 'Saul Goodman',
+        lat: 0,
+        lon: 0,
+        description: 'good girl',
+        lost: 0,
+      }
+    ]
+  })
+}
