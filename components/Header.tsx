badd38faec375e39b83a6d4416a5445bbf6255af
Arthur <arthur.mello.marques@gmail.com>
2022-08-01 15:15:04 -0300

    refactor: separate footer and header components

---
+++ components/Header.tsx
@@ -0,0 +1,33 @@
+import Link from 'next/link'
+import styles from '../styles/header.module.css'
+
+export default function Header() {
+    return (<div className={styles.header}>
+      <div className={styles.logo}>
+        Logo
+      </div>
+      <div className={styles.linksContainer}>
+        <Link href="#">
+          <a className={styles.link}>
+            Encontrei um pet {/*Ia ser interessante se esses dois itens fossem "animais encontrados" e "animais perdidos",
+                                e esses "encontrei/perdi um pet" fossem botões na tela que permitissem criar uma occurrence*/}
+          </a>
+        </Link>
+        <Link href="#">
+          <a className={styles.link}>
+            Perdi meu pet
+          </a>
+        </Link>
+        <Link href="#">
+          <a className={styles.link}>
+            Me avise sobre pets perdidos na minha área
+          </a>
+        </Link>
+        <Link href="#">
+          <a className={styles.link}>
+            Imprimir pôster de animal perdido
+          </a>
+        </Link>
+      </div>
+    </div>)
+  }
