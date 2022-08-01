8ac1542a93f9763b50dae872667488e64c4073eb
Arthur <arthur.mello.marques@gmail.com>
2022-08-01 15:15:04 -0300

    refactor: separate footer and header components

---
+++ components/Footer.tsx
@@ -0,0 +1,6 @@
+
+export default function Footer() {
+    return (<p>
+    this will show some information about the app, maybe a few links related to pet adoption centers, animal rescue organizations, etc
+    </p>)
+  }
