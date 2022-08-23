f16478de1fa5d779574af9c2e49b6ef82641d7c2
Arthur <arthur.mello.marques@gmail.com>
2022-08-23 16:57:42 -0300

    feat: handle form submission on front and backend

--- pages/api/writeOccurrence.ts
+++ pages/api/writeOccurrence.ts
@@ -1,23 +1,26 @@
 import { PrismaClient, Prisma } from "@prisma/client";
 const prisma = new PrismaClient();
 
-export default function handler(req, res) {
-  const occurrence: Prisma.OccurrenceCreateArgs = req.body.occurrence;
+export default async function handler(req, res) {
+  const parameters = JSON.parse(req.body);
+  const occurrence: Prisma.OccurrenceCreateInput = {
+      petName: parameters.petName as string,
+      author: parameters.author as string,
+      lat: Number(parameters.lat),
+      lon: Number(parameters.lon),
+      petDescription: parameters.petDescription as string
+  }
 
-  writeOccurrence(occurrence).then(async () => {
-    await prisma.$disconnect();
-    return res.status(200).json({});
-  }).catch(async (error) => {
-    await prisma.$disconnect();
-    return res.status(500).json({
-      error
+  try {
+    await prisma.occurrence.create({
+      data: occurrence
     });
-  });
-}
 
-async function writeOccurrence(occurrence: Prisma.OccurrenceCreateArgs) {
-  await prisma.occurrence.create({
-    data:
-    occurrence
-  });
+    await prisma.$disconnect();
+    return res.status(200).end();
+  } catch (error) {
+    await prisma.$disconnect();
+    console.error(error);
+    return res.status(500).send(error);
+  };
 }
